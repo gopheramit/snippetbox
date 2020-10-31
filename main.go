7b2e19b84d63f15ed6ff8a2033d5c78144a55ea5
gopheramit <amitagrawal10@outlook.com>
2020-10-31 18:49:37 +0530

    routing routes

--- main.go
+++ main.go
@@ -6,13 +6,27 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from snippetbox"))
 }
 
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Showing snippet"))
+
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("create new snippet"))
+}
 func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting server on:4000")
 	err := http.ListenAndServe(":4000", mux)
