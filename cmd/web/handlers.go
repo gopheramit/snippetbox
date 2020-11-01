d1d4de433605cc9dd8284cc6dcc30f2f60b09d06
gopheramit <amitagrawal10@outlook.com>
2020-11-01 16:04:39 +0530

    reverting to static seerver file

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,57 +2,52 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	
 	"html/template"
+	"log"
+	"net/http"
 	"strconv"
 )
 
-func (app *application) home(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		//http.NotFound(w, r)
-		app.notFound(w)
+		http.NotFound(w, r)
 		return
 	}
-	files :=[]string{
+	files := []string{
 		"./ui/html/home.page.tmpl",
 		"./ui/html/base.layout.tmpl",
 		"./ui/html/footer.partial.tmpl",
 	}
 
-	ts,err:=template.ParseFiles(files...)
-	if err !=nil{
-		//log.Println(err.Error())
-		//http.Error(w,"internal server error ",500)
-		app.serverError(w,err)
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "internal server error ", 500)
 		return
 	}
-	err =ts.Execute(w,nil)
-	if err!=nil{
-		//log.Println(err.Error())
-		//http.Error(w,"internal server error ",500)
-		app.serverError(w,err)
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "internal server error ", 500)
 		return
 	}
-	
+
 }
 
-func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+func showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		//http.NotFound(w, r)
-		app.notFound(w)
+		http.NotFound(w, r)
 		return
 	}
 	//w.Write([]byte("Showing snippet"))
 	fmt.Fprintf(w, "Display a specific snippet with id %d", id)
 }
 
-func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+func createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		//http.Error(w, "Method not allowed ", 405)
-		app.clientError(w,http.StatusMethodNotAllowed)
+		http.Error(w, "Method not allowed ", 405)
 		return
 	}
 	w.Write([]byte("create new snippet"))
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,48 +1,21 @@
 package main
 
 import (
-	"os"
-	"flag"
-	
 	"log"
 	"net/http"
-	
 )
 
-type application struct {
-	errorLog *log.Logger
-	infoLog *log.Logger
-}
-
-
 func main() {
-	addr:=flag.String("addr","8000","HTTP network address")
-	flag.Parse()
+
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
-	infoLog:=log.New(os.Stdout,"INFO/t",log.Ldate|log.Ltime)
-	errorLog:=log.New(os.Stderr,"ERROR/t",log.Ldate|log.Ltime|log.Lshortfile)
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
-	app := & application{
-		errorLog:errorLog,
-		infoLog:infoLog,
-	}
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	fileServer:=http.FileServer(http.Dir("./ui/static/"))
-
-
-
-	mux.Handle("/static/",http.StripPrefix("/static",fileServer))
-
-	srv :=&http.Server{
-		Addr: *addr,
-		ErrorLog:errorLog,
-		Handler:mux,
-	}
-	infoLog.Printf("Starting server on %s",*addr)
-	err := srv.ListenAndServe()
-	errorLog.Fatal(err)
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	log.Println("Starting server on:8000")
+	err := http.ListenAndServe(":8000", mux)
+	log.Fatal(err)
 }
