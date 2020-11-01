9d03d41f93645f15c5eed25dbc856d9a780fc269
gopheramit <amitagrawal10@outlook.com>
2020-11-01 16:15:26 +0530

    command line flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
@@ -11,11 +12,13 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	log.Println("Starting server on:8000")
-	err := http.ListenAndServe(":8000", mux)
+
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
