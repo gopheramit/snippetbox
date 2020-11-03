ccf1338e92aa709aa3c2864ab72cb3daf7924e71
gopheramit <amitagrawal10@outlook.com>
2020-11-03 21:59:54 +0530

    restful routing

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,10 +11,10 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
+	/*if r.URL.Path != "/" {
 		app.notFound(w)
 		return
-	}
+	}*/
 	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -26,7 +26,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 }
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -46,16 +46,19 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	title := "O snail"
 	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
 	expires := "7"
 
-	if r.Method != http.MethodPost {
+	/*if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
-	}
+	}*/
 
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
