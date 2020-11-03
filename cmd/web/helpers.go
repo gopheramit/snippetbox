4f012345e671f802651a17faed5e575fc75b03ec
gopheramit <amitagrawal10@outlook.com>
2020-11-03 20:06:22 +0530

    custom template function

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,8 +5,16 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
+	td.CurrentYear = time.Now().Year()
+	return td
+}
 func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
 	buf := new(bytes.Buffer)
 
@@ -16,7 +24,7 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 		return
 	}
 	// Execute the template set, passing in any dynamic data.
-	err := ts.Execute(buf, td)
+	err := ts.Execute(buf, app.addDefaultData(td, r))
 	if err != nil {
 		app.serverError(w, err)
 		return
